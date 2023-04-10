da6df714f4bce532177346f9d373d7acad6abda7
jenikinsdocker <jenikinsdocker@gmail.com>
2023-04-10 11:05:01 +0530

    first test commit

--- src/main/java/com/boot/rest/SpringBootRestApplication.java
+++ src/main/java/com/boot/rest/SpringBootRestApplication.java
@@ -8,7 +8,7 @@ public class SpringBootRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApplication.class, args);
-		System.out.println("main commit aaasfdd");
+		System.out.println();
 	}
 
 }
